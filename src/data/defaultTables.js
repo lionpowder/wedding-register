bfb81adb4066d17925bddf71ce8596d5b5f8bf81
lionpowder <hanna7810@hotmail.com>
2024-01-05 13:45:46 +0800

    default table data

--- src/data/defaultTables.js
+++ src/data/defaultTables.js
@@ -96,8 +96,8 @@ export const defaultTables = [
     },
   },
   {
-    tableCode: "1-1",
-    tableName: "女方親友-1",
+    tableCode: "2-1",
+    tableName: "女方親友-2",
     reservedSeats: {
       regular: {
         actual: 0,
@@ -128,8 +128,40 @@ export const defaultTables = [
     },
   },
   {
-    tableCode: "1-1",
-    tableName: "女方親友-1",
+    tableCode: "2-2",
+    tableName: "女方同學",
+    reservedSeats: {
+      regular: {
+        actual: 0,
+        target: 10,
+      },
+      vegetarian: {
+        actual: 0,
+        target: 1,
+      },
+      childSeat: {
+        actual: 0,
+        target: 0,
+      },
+    },
+    nonreservedSeats: {
+      regular: {
+        actual: 0,
+        target: 0,
+      },
+      vegetarian: {
+        actual: 0,
+        target: 0,
+      },
+      childSeat: {
+        actual: 0,
+        target: 0,
+      },
+    },
+  },
+  {
+    tableCode: "2-3",
+    tableName: "吐司-1",
     reservedSeats: {
       regular: {
         actual: 0,
@@ -160,8 +192,8 @@ export const defaultTables = [
     },
   },
   {
-    tableCode: "1-1",
-    tableName: "女方親友-1",
+    tableCode: "2-5",
+    tableName: "吐司-3",
     reservedSeats: {
       regular: {
         actual: 0,
@@ -192,8 +224,40 @@ export const defaultTables = [
     },
   },
   {
-    tableCode: "1-1",
-    tableName: "女方親友-1",
+    tableCode: "3-1",
+    tableName: "女方親友-3",
+    reservedSeats: {
+      regular: {
+        actual: 0,
+        target: 10,
+      },
+      vegetarian: {
+        actual: 0,
+        target: 0,
+      },
+      childSeat: {
+        actual: 0,
+        target: 1,
+      },
+    },
+    nonreservedSeats: {
+      regular: {
+        actual: 0,
+        target: 0,
+      },
+      vegetarian: {
+        actual: 0,
+        target: 0,
+      },
+      childSeat: {
+        actual: 0,
+        target: 0,
+      },
+    },
+  },
+  {
+    tableCode: "3-2",
+    tableName: "女方親友-4",
     reservedSeats: {
       regular: {
         actual: 0,
@@ -224,8 +288,40 @@ export const defaultTables = [
     },
   },
   {
-    tableCode: "1-1",
-    tableName: "女方親友-1",
+    tableCode: "3-3",
+    tableName: "女方/男方親友",
+    reservedSeats: {
+      regular: {
+        actual: 0,
+        target: 6,
+      },
+      vegetarian: {
+        actual: 0,
+        target: 0,
+      },
+      childSeat: {
+        actual: 0,
+        target: 0,
+      },
+    },
+    nonreservedSeats: {
+      regular: {
+        actual: 0,
+        target: 4,
+      },
+      vegetarian: {
+        actual: 0,
+        target: 0,
+      },
+      childSeat: {
+        actual: 0,
+        target: 0,
+      },
+    },
+  },
+  {
+    tableCode: "3-5",
+    tableName: "吐司-2",
     reservedSeats: {
       regular: {
         actual: 0,
@@ -256,12 +352,364 @@ export const defaultTables = [
     },
   },
   {
-    tableCode: "1-1",
-    tableName: "女方親友-1",
+    tableCode: "5-1",
+    tableName: "男方爸爸親友-1",
+    reservedSeats: {
+      regular: {
+        actual: 0,
+        target: 0,
+      },
+      vegetarian: {
+        actual: 0,
+        target: 0,
+      },
+      childSeat: {
+        actual: 0,
+        target: 0,
+      },
+    },
+    nonreservedSeats: {
+      regular: {
+        actual: 0,
+        target: 10,
+      },
+      vegetarian: {
+        actual: 0,
+        target: 0,
+      },
+      childSeat: {
+        actual: 0,
+        target: 0,
+      },
+    },
+  },
+  {
+    tableCode: "5-2",
+    tableName: "男方爸爸親友-2",
+    reservedSeats: {
+      regular: {
+        actual: 0,
+        target: 0,
+      },
+      vegetarian: {
+        actual: 0,
+        target: 0,
+      },
+      childSeat: {
+        actual: 0,
+        target: 0,
+      },
+    },
+    nonreservedSeats: {
+      regular: {
+        actual: 0,
+        target: 10,
+      },
+      vegetarian: {
+        actual: 0,
+        target: 0,
+      },
+      childSeat: {
+        actual: 0,
+        target: 0,
+      },
+    },
+  },
+  {
+    tableCode: "5-3",
+    tableName: "男方爸爸親友-4",
     reservedSeats: {
       regular: {
         actual: 0,
         target: 10,
+      },
+      vegetarian: {
+        actual: 0,
+        target: 1,
+      },
+      childSeat: {
+        actual: 0,
+        target: 0,
+      },
+    },
+    nonreservedSeats: {
+      regular: {
+        actual: 0,
+        target: 10,
+      },
+      vegetarian: {
+        actual: 0,
+        target: 0,
+      },
+      childSeat: {
+        actual: 0,
+        target: 0,
+      },
+    },
+  },
+  {
+    tableCode: "5-5",
+    tableName: "男方國中同學/家長/老師",
+    reservedSeats: {
+      regular: {
+        actual: 0,
+        target: 0,
+      },
+      vegetarian: {
+        actual: 0,
+        target: 1,
+      },
+      childSeat: {
+        actual: 0,
+        target: 0,
+      },
+    },
+    nonreservedSeats: {
+      regular: {
+        actual: 0,
+        target: 10,
+      },
+      vegetarian: {
+        actual: 0,
+        target: 0,
+      },
+      childSeat: {
+        actual: 0,
+        target: 0,
+      },
+    },
+  },
+  {
+    tableCode: "6-1",
+    tableName: "男方媽媽親友-1",
+    reservedSeats: {
+      regular: {
+        actual: 0,
+        target: 0,
+      },
+      vegetarian: {
+        actual: 0,
+        target: 0,
+      },
+      childSeat: {
+        actual: 0,
+        target: 0,
+      },
+    },
+    nonreservedSeats: {
+      regular: {
+        actual: 0,
+        target: 10,
+      },
+      vegetarian: {
+        actual: 0,
+        target: 0,
+      },
+      childSeat: {
+        actual: 0,
+        target: 0,
+      },
+    },
+  },
+  {
+    tableCode: "6-2",
+    tableName: "男方爸爸親友-3",
+    reservedSeats: {
+      regular: {
+        actual: 0,
+        target: 3,
+      },
+      vegetarian: {
+        actual: 0,
+        target: 0,
+      },
+      childSeat: {
+        actual: 0,
+        target: 1,
+      },
+    },
+    nonreservedSeats: {
+      regular: {
+        actual: 0,
+        target: 7,
+      },
+      vegetarian: {
+        actual: 0,
+        target: 0,
+      },
+      childSeat: {
+        actual: 0,
+        target: 0,
+      },
+    },
+  },
+  {
+    tableCode: "6-3",
+    tableName: "男方爸爸親友-5",
+    reservedSeats: {
+      regular: {
+        actual: 0,
+        target: 3,
+      },
+      vegetarian: {
+        actual: 0,
+        target: 0,
+      },
+      childSeat: {
+        actual: 0,
+        target: 0,
+      },
+    },
+    nonreservedSeats: {
+      regular: {
+        actual: 0,
+        target: 8,
+      },
+      vegetarian: {
+        actual: 0,
+        target: 0,
+      },
+      childSeat: {
+        actual: 0,
+        target: 0,
+      },
+    },
+  },
+  {
+    tableCode: "6-5",
+    tableName: "男方爸爸親友-6",
+    reservedSeats: {
+      regular: {
+        actual: 0,
+        target: 2,
+      },
+      vegetarian: {
+        actual: 0,
+        target: 0,
+      },
+      childSeat: {
+        actual: 0,
+        target: 0,
+      },
+    },
+    nonreservedSeats: {
+      regular: {
+        actual: 0,
+        target: 9,
+      },
+      vegetarian: {
+        actual: 0,
+        target: 0,
+      },
+      childSeat: {
+        actual: 0,
+        target: 0,
+      },
+    },
+  },
+  {
+    tableCode: "7-1",
+    tableName: "男方媽媽親友-2",
+    reservedSeats: {
+      regular: {
+        actual: 0,
+        target: 0,
+      },
+      vegetarian: {
+        actual: 0,
+        target: 0,
+      },
+      childSeat: {
+        actual: 0,
+        target: 0,
+      },
+    },
+    nonreservedSeats: {
+      regular: {
+        actual: 0,
+        target: 10,
+      },
+      vegetarian: {
+        actual: 0,
+        target: 0,
+      },
+      childSeat: {
+        actual: 0,
+        target: 0,
+      },
+    },
+  },
+  {
+    tableCode: "7-2",
+    tableName: "男方媽媽親友-3",
+    reservedSeats: {
+      regular: {
+        actual: 0,
+        target: 1,
+      },
+      vegetarian: {
+        actual: 0,
+        target: 1,
+      },
+      childSeat: {
+        actual: 0,
+        target: 0,
+      },
+    },
+    nonreservedSeats: {
+      regular: {
+        actual: 0,
+        target: 9,
+      },
+      vegetarian: {
+        actual: 0,
+        target: 0,
+      },
+      childSeat: {
+        actual: 0,
+        target: 0,
+      },
+    },
+  },
+  {
+    tableCode: "7-3",
+    tableName: "男方爸媽親友",
+    reservedSeats: {
+      regular: {
+        actual: 0,
+        target: 0,
+      },
+      vegetarian: {
+        actual: 0,
+        target: 0,
+      },
+      childSeat: {
+        actual: 0,
+        target: 0,
+      },
+    },
+    nonreservedSeats: {
+      regular: {
+        actual: 0,
+        target: 10,
+      },
+      vegetarian: {
+        actual: 0,
+        target: 0,
+      },
+      childSeat: {
+        actual: 0,
+        target: 0,
+      },
+    },
+  },
+  {
+    tableCode: "7-5",
+    tableName: "預備桌",
+    reservedSeats: {
+      regular: {
+        actual: 0,
+        target: 0,
       },
       vegetarian: {
         actual: 0,
@@ -288,456 +736,8 @@ export const defaultTables = [
     },
   },
   {
-    tableCode: "1-1",
-    tableName: "女方親友-1",
-    reservedSeats: {
-      regular: {
-        actual: 0,
-        target: 10,
-      },
-      vegetarian: {
-        actual: 0,
-        target: 0,
-      },
-      childSeat: {
-        actual: 0,
-        target: 0,
-      },
-    },
-    nonreservedSeats: {
-      regular: {
-        actual: 0,
-        target: 0,
-      },
-      vegetarian: {
-        actual: 0,
-        target: 0,
-      },
-      childSeat: {
-        actual: 0,
-        target: 0,
-      },
-    },
-  },
-  {
-    tableCode: "1-1",
-    tableName: "女方親友-1",
-    reservedSeats: {
-      regular: {
-        actual: 0,
-        target: 10,
-      },
-      vegetarian: {
-        actual: 0,
-        target: 0,
-      },
-      childSeat: {
-        actual: 0,
-        target: 0,
-      },
-    },
-    nonreservedSeats: {
-      regular: {
-        actual: 0,
-        target: 0,
-      },
-      vegetarian: {
-        actual: 0,
-        target: 0,
-      },
-      childSeat: {
-        actual: 0,
-        target: 0,
-      },
-    },
-  },
-  {
-    tableCode: "1-1",
-    tableName: "女方親友-1",
-    reservedSeats: {
-      regular: {
-        actual: 0,
-        target: 10,
-      },
-      vegetarian: {
-        actual: 0,
-        target: 0,
-      },
-      childSeat: {
-        actual: 0,
-        target: 0,
-      },
-    },
-    nonreservedSeats: {
-      regular: {
-        actual: 0,
-        target: 0,
-      },
-      vegetarian: {
-        actual: 0,
-        target: 0,
-      },
-      childSeat: {
-        actual: 0,
-        target: 0,
-      },
-    },
-  },
-  {
-    tableCode: "1-1",
-    tableName: "女方親友-1",
-    reservedSeats: {
-      regular: {
-        actual: 0,
-        target: 10,
-      },
-      vegetarian: {
-        actual: 0,
-        target: 0,
-      },
-      childSeat: {
-        actual: 0,
-        target: 0,
-      },
-    },
-    nonreservedSeats: {
-      regular: {
-        actual: 0,
-        target: 0,
-      },
-      vegetarian: {
-        actual: 0,
-        target: 0,
-      },
-      childSeat: {
-        actual: 0,
-        target: 0,
-      },
-    },
-  },
-  {
-    tableCode: "1-1",
-    tableName: "女方親友-1",
-    reservedSeats: {
-      regular: {
-        actual: 0,
-        target: 10,
-      },
-      vegetarian: {
-        actual: 0,
-        target: 0,
-      },
-      childSeat: {
-        actual: 0,
-        target: 0,
-      },
-    },
-    nonreservedSeats: {
-      regular: {
-        actual: 0,
-        target: 0,
-      },
-      vegetarian: {
-        actual: 0,
-        target: 0,
-      },
-      childSeat: {
-        actual: 0,
-        target: 0,
-      },
-    },
-  },
-  {
-    tableCode: "1-1",
-    tableName: "女方親友-1",
-    reservedSeats: {
-      regular: {
-        actual: 0,
-        target: 10,
-      },
-      vegetarian: {
-        actual: 0,
-        target: 0,
-      },
-      childSeat: {
-        actual: 0,
-        target: 0,
-      },
-    },
-    nonreservedSeats: {
-      regular: {
-        actual: 0,
-        target: 0,
-      },
-      vegetarian: {
-        actual: 0,
-        target: 0,
-      },
-      childSeat: {
-        actual: 0,
-        target: 0,
-      },
-    },
-  },
-  {
-    tableCode: "1-1",
-    tableName: "女方親友-1",
-    reservedSeats: {
-      regular: {
-        actual: 0,
-        target: 10,
-      },
-      vegetarian: {
-        actual: 0,
-        target: 0,
-      },
-      childSeat: {
-        actual: 0,
-        target: 0,
-      },
-    },
-    nonreservedSeats: {
-      regular: {
-        actual: 0,
-        target: 0,
-      },
-      vegetarian: {
-        actual: 0,
-        target: 0,
-      },
-      childSeat: {
-        actual: 0,
-        target: 0,
-      },
-    },
-  },
-  {
-    tableCode: "1-1",
-    tableName: "女方親友-1",
-    reservedSeats: {
-      regular: {
-        actual: 0,
-        target: 10,
-      },
-      vegetarian: {
-        actual: 0,
-        target: 0,
-      },
-      childSeat: {
-        actual: 0,
-        target: 0,
-      },
-    },
-    nonreservedSeats: {
-      regular: {
-        actual: 0,
-        target: 0,
-      },
-      vegetarian: {
-        actual: 0,
-        target: 0,
-      },
-      childSeat: {
-        actual: 0,
-        target: 0,
-      },
-    },
-  },
-  {
-    tableCode: "1-1",
-    tableName: "女方親友-1",
-    reservedSeats: {
-      regular: {
-        actual: 0,
-        target: 10,
-      },
-      vegetarian: {
-        actual: 0,
-        target: 0,
-      },
-      childSeat: {
-        actual: 0,
-        target: 0,
-      },
-    },
-    nonreservedSeats: {
-      regular: {
-        actual: 0,
-        target: 0,
-      },
-      vegetarian: {
-        actual: 0,
-        target: 0,
-      },
-      childSeat: {
-        actual: 0,
-        target: 0,
-      },
-    },
-  },
-  {
-    tableCode: "1-1",
-    tableName: "女方親友-1",
-    reservedSeats: {
-      regular: {
-        actual: 0,
-        target: 10,
-      },
-      vegetarian: {
-        actual: 0,
-        target: 0,
-      },
-      childSeat: {
-        actual: 0,
-        target: 0,
-      },
-    },
-    nonreservedSeats: {
-      regular: {
-        actual: 0,
-        target: 0,
-      },
-      vegetarian: {
-        actual: 0,
-        target: 0,
-      },
-      childSeat: {
-        actual: 0,
-        target: 0,
-      },
-    },
-  },
-  {
-    tableCode: "1-1",
-    tableName: "女方親友-1",
-    reservedSeats: {
-      regular: {
-        actual: 0,
-        target: 10,
-      },
-      vegetarian: {
-        actual: 0,
-        target: 0,
-      },
-      childSeat: {
-        actual: 0,
-        target: 0,
-      },
-    },
-    nonreservedSeats: {
-      regular: {
-        actual: 0,
-        target: 0,
-      },
-      vegetarian: {
-        actual: 0,
-        target: 0,
-      },
-      childSeat: {
-        actual: 0,
-        target: 0,
-      },
-    },
-  },
-  {
-    tableCode: "1-1",
-    tableName: "女方親友-1",
-    reservedSeats: {
-      regular: {
-        actual: 0,
-        target: 10,
-      },
-      vegetarian: {
-        actual: 0,
-        target: 0,
-      },
-      childSeat: {
-        actual: 0,
-        target: 0,
-      },
-    },
-    nonreservedSeats: {
-      regular: {
-        actual: 0,
-        target: 0,
-      },
-      vegetarian: {
-        actual: 0,
-        target: 0,
-      },
-      childSeat: {
-        actual: 0,
-        target: 0,
-      },
-    },
-  },
-  {
-    tableCode: "1-1",
-    tableName: "女方親友-1",
-    reservedSeats: {
-      regular: {
-        actual: 0,
-        target: 10,
-      },
-      vegetarian: {
-        actual: 0,
-        target: 0,
-      },
-      childSeat: {
-        actual: 0,
-        target: 0,
-      },
-    },
-    nonreservedSeats: {
-      regular: {
-        actual: 0,
-        target: 0,
-      },
-      vegetarian: {
-        actual: 0,
-        target: 0,
-      },
-      childSeat: {
-        actual: 0,
-        target: 0,
-      },
-    },
-  },
-  {
-    tableCode: "1-1",
-    tableName: "女方親友-1",
-    reservedSeats: {
-      regular: {
-        actual: 0,
-        target: 10,
-      },
-      vegetarian: {
-        actual: 0,
-        target: 0,
-      },
-      childSeat: {
-        actual: 0,
-        target: 0,
-      },
-    },
-    nonreservedSeats: {
-      regular: {
-        actual: 0,
-        target: 0,
-      },
-      vegetarian: {
-        actual: 0,
-        target: 0,
-      },
-      childSeat: {
-        actual: 0,
-        target: 0,
-      },
-    },
-  },
-  {
-    tableCode: "1-1",
-    tableName: "女方親友-1",
+    tableCode: "主",
+    tableName: "主桌",
     reservedSeats: {
       regular: {
         actual: 0,
